bde0cb3b540bc0cd219faa2444dff2963d16e175
ashwinky <ashwin.yka@gmail.com>
2022-08-08 09:35:57 +0530

    fmt: fix macro structure and add bsort docs
    Signed-off-by: ashwinky <ashwin.yka@gmail.com>

--- lib/sort.h
+++ lib/sort.h
@@ -1,19 +1,22 @@
 #ifndef SEM3_DSA_C_SORT_H
 #define SEM3_DSA_C_SORT_H
 
-#endif // SEM3_DSA_C_SORT_H
-
 // Bubble Sort
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
+// This algorithm will sort the array in ascending order,
+// using the bubble sort algorithm.
 void bsort(int array[], int size) {
+  int temp;
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size - 1; j++) {
       if (array[j] > array[j + 1]) {
-        int temp = array[j];
+        temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
     }
   }
 }
+
+#endif // SEM3_DSA_C_SORT_H
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include "lib/search.h"
 #include <stdio.h>
+
+#include "lib/search.h"
+#include "lib/sort.h"
 
 int main() {
   int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
