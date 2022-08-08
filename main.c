5c51eb8c2c918261f8b9f88b574c14f4152b629a
ashwinky <ashwin.yka@gmail.com>
2022-08-08 16:27:29 +0530

    lib: reset main.c
    Signed-off-by: ashwinky <ashwin.yka@gmail.com>

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
-#include "lib/search.h"
-#include "lib/sort.h"
-
 int main() {
-  int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-  int i = bsearch(arr, sizeof(arr) / sizeof(arr[0]), 3);
-  int j = lsearch(arr, sizeof(arr) / sizeof(arr[0]), 3);
-
-  printf("%d %d\n", i, j);
+  printf("Hello World\n");
 
   return 0;
 }
